4a88802b99ad32d8cb47182e83e39b2750b84a78
1 <a@1deiMac-4.local>
2018-04-13 18:03:02 +0800

    add style

--- src/components/main-header/head.vue
+++ src/components/main-header/head.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <span>header</span>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">music</h1>
   </div>
-
 </template>
 
 <script>
@@ -22,6 +22,27 @@
 
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/variable.styl"
+  @import "../../common/stylus/mixin.styl"
+  .m-header
+    position relative
+    height 44px
+    background-color darkcyan
+    text-align center
+    font-size 0
+    .icon
+      display inline-block
+      vertical-align top
+      margin-top 6px
+      width 30px
+      height 30px
+      bg-image('logo')
+      background-size 30px 30px
+    .text
+      display inline-block
+      vertical-align top
+      line-height 44px
+      font-size 16px
+      background-color aliceblue
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
 
+// import './common/stylus/index.styl'
+
 Vue.config.productionTip = false
 
 fastclick.attach(document.body)
